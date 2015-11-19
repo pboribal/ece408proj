4905ab7a59b2fe311b1b6bcc4ec8656e29045475
Perut <boribal2@illinois.edu>
2015-11-19 15:51:10 -0600

    improved development pipeline

--- CodeSketches/testcasegen.cpp
+++ CodeSketches/testcasegen.cpp
@@ -1,47 +1,40 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <cmath>
+#include <algorithm>
+#include <vector>
+
+typedef std::pair<float,int> elem;
+typedef struct {
+	bool operator()(const elem& a, const elem& b)
+	{
+		return std::abs(a.first) > std::abs(b.first) || (std::abs(a.first)==std::abs(b.first) && a.second < b.second );
+	}
+} elemcmp;
+
 int main()
 {
-	srand(time(NULL));
+	std::srand(std::time(NULL));
 	int n;
 	int k;
 	std::cin >> n;
 	std::cin >> k;
 	std::cout << n << " " << k << std::endl;
-	float f[n];
-	int idx[n];
+	std::vector<elem> f;
 	for(int i=0;i<n;i++)
 	{
-		f[i] = (0.5-((float)rand())/((float)RAND_MAX))*20;
-		idx[i] = i;
-		std::cout << f[i] << std::endl;
+		elem fi;
+		fi.first = (0.5-((float)std::rand())/((float)RAND_MAX))*20;
+		fi.second = i;
+		std::cout << fi.first << std::endl;
+		f.push_back(fi);	
 	}
 	std::cout << std::endl;
-	// Bubble sort is enough, it's just a test case generator
-	// no need to be performance critical here
-	volatile bool swapped = false;
-	do
-	{
-		swapped = false;
-		for(int i=1;i<n;i++)
-		{
-			if( std::abs(f[i-1]) < std::abs(f[i]) )
-			{
-				float t = f[i-1];
-				f[i-1] = f[i];
-				f[i] = t;
-				int j = idx[i-1];
-				idx[i-1] = idx[i];
-				idx[i] = j;
-				swapped = true;
-			}
-		}		
-	}while(swapped);
-
+	elemcmp e;
+	std::sort(f.begin(),f.end(),e);
 	for(int i=0;i<k;i++)
 	{
-		std::cout << idx[i] << "  " << f[i] << std::endl;
+		std::cout << f[i].second << "  " << f[i].first << std::endl;
 	}
 	return 0;
 }
